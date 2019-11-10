4a47bdeec058fcec4112d20c8f190f6fc7dbcb32
hoanhtuds <hoanhtuds@gmail.com>
2019-11-11 00:22:20 +0700

    change2

---
+++ TankGame/Assets/Scripts/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float speed;
+    public float lifeTime;
+
+    public GameObject destroyEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("DestroyProjectile",lifeTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(transform.up * speed * Time.deltaTime);
+    }
+
+    void DestroyProjectile()
+    {
+        Instantiate(destroyEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+
+    }
+}
---
+++ TankGame/Assets/Scripts/TankController2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController2 : MonoBehaviour
+{
+    public Rigidbody2D Rb;
+    private float speed=6;
+    private float velx;
+    private float vely;
+    private Animator anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        Rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velx = Input.GetAxis("Horizontal");
+        vely = Rb.velocity.y;
+        Rb.velocity = new Vector2(velx*speed,vely);
+
+        if(velx == 0)
+        {
+            anim.SetBool("isRunning", false);
+        }
+        else
+            anim.SetBool("isRunning", true);
+    }
+}
